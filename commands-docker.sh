a95844d91cf3cbac3a5e175161683a71b5a6f64c
Rachid Ben Moussa <rachid.benmoussa@luminis.eu>
2015-09-08 21:27:41 +0200

    Added docker commands

--- commands-docker.sh
+++ commands-docker.sh
@@ -1,3 +1,6 @@
+## init 
+$(boot2docker shellinit)
+
 
 ## List running containers:
 docker ps -f "status=running"
@@ -10,3 +13,12 @@ docker stop `docker ps -q -f "status=running"`
 
 ## Stop container latest started: 
 docker stop `docker ps -q  -f "status=running" -n 1`
+
+
+
+## Expose ports
+for i in {9000..9001}; do
+VBoxManage modifyvm "boot2docker-vm" --natpf1 "tcp-port$i,tcp,,$i,,$i";
+VBoxManage modifyvm "boot2docker-vm" --natpf1 "udp-port$i,udp,,$i,,$i";
+done
+
