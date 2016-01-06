6c8ac7bce78dd328139c1f1752d42ae745646e0b
Rachid Ben Moussa <Rachid.BenMoussa@planonsoftware.com>
2016-01-06 14:51:34 +0100

    Added Gradle oneliner

--- groovy1liners.groovy
+++ groovy1liners.groovy
@@ -1,7 +1,11 @@
 
 
-	file(".").listFiles()
-		.findAll {it.name.endsWith('.bndrun')}
-		.collect{ it.name.substring(0, it.name.lastIndexOf(".")) }	// Strip extension
-		.each{ println it }
+file(".").listFiles()
+	.findAll {it.name.endsWith('.bndrun')}
+	.collect{ it.name.substring(0, it.name.lastIndexOf(".")) }	// Strip extension
+	.each{ println it }
 
+
+configurations.findAll().each { config ->
+	config.allArtifacts.getFiles().each { file -> println "$file.name     -     $config"  }
+}
