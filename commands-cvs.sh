5527d18af0b5cf2b1fe3879448bcce85873b3108
Rachid Ben Moussa <rachid.benmoussa@planonsoftware.com>
2016-07-01 17:03:19 +0200

    update from Windows

--- commands-cvs.sh
+++ commands-cvs.sh
@@ -1,17 +1,17 @@
 https://www-e815.fnal.gov/webspace/cvs/commands.html
 
-
-cvs history -a -c  -D 2016-05-01 -f api.bnd
-
--n AgileWorkplaceManagement 
-
-cvs update -kb thefile
-cvs commit -fm "Change substitution mode" thefile
-
 ## Checkout 
 export CVSROOT=user@bla.nl:/vol/cvs
+cvs login
 cvs co project
 
 ## Remove all files
 find . -type f ! -path \*CVS\* -exec rm {} \; -exec cvs remove {} \;\
+
+
+
+cvs history -a -c  -D 2016-05-01 -f api.bnd
+
+cvs update -kb thefile
+cvs commit -fm "Change substitution mode" thefile
 
