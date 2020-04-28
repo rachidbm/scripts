53329693641df30b0cd1a120eaeda208fdab5585
Rachid  Ben Moussa <rbenmoussa@bol.com>
2020-04-28 08:57:31 +0200

    Updated bash_aliases and brew packages

--- osx/brew-install-packages.sh
+++ osx/brew-install-packages.sh
@@ -1,26 +1,30 @@
 brew install \
  vim npm wget nmap pv p7zip xz watch jq hh rsync svn libusb
 
-brew cask install osxfuse
-brew install ntfs-3g
-brew cask install mounty
+brew cask install copyclip sublime-text brave-browser
+
+# brew cask install insomnia
+
+## Install atom
+brew cask install atom && apm install compare-files
+
+# brew cask install osxfuse
+# brew install ntfs-3g
+# brew cask install mounty
 
 # brew tap aws/tap
 # brew install aws-sam-cli
 
-# brew cask install copyclip
 
-## For ESP development: 
+## For ESP development:
 # brew tap adrianmihalko/ch340g-ch34g-ch34x-mac-os-x-driver https://github.com/adrianmihalko/ch340g-ch34g-ch34x-mac-os-x-driver
 # brew cask install wch-ch34x-usb-serial-driver
-
 ## Mongoose OS
 # brew tap cesanta/mos && brew install mos
 # brew cask install fritzing
 
-
 ## KeepPassXC
-# brew cask install keepassxc 
+# brew cask install keepassxc
 
 
 # brew cask install calibre
